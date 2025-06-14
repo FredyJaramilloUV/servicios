1263f1049812514445fa6a50773cde18736b8136
Fredy Jaramillo <jaramillo.fredy@gmail.com>
2025-06-14 12:46:09 -0500

    V 0.0.1 feat (Posts) lee posts de interner y los lista  On branch main  Changes to be committed: 	modified:   src/App.vue 	new file:   src/components/PostList.vue 	new file:   src/services/PostService.js DONE se hizo una prueba de uso de servicios para usar API rest TODO None

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PostList />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+  import PostList from './components/PostList.vue'
 </script>
 
 <style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,19 @@
+<template>
+<ul class="post-list">
+  <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+</ul>
+</template>
+
+<script setup>
+  import PostService from '@/services/PostService'
+  import { onMounted } from 'vue'
+  const  service = new PostService()
+  const posts = service.getPosts()
+
+  onMounted(async () => {
+    await service.fetchAll()
+  })
+</script>
+
+<style scoped>
+</style>
